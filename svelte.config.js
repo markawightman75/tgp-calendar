57aa8832510d65746ddd35a386c47a340eeb76ce
Mark Wightman <mark.wightman@aluma.io>
2025-06-21 13:44:34 +0100

    Configure GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/tgp-calendar' : ''
+		}
+	}
 };
 
 export default config;
